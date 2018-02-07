d0e555775e4b4931b3a1fe95db35d719f267594d
Ryan LaRouche <rwlarouche@yahoo.com>
2018-02-07 11:34:36 -0500

    Uploaded initial GameEngine and Pacman projects

---
+++ src/GameEngine2/src/org/game/engine/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Package contains classes used to run the basic Game Engine
+ * including the Graphical interface
+ * 
+ * @author 		Ryan LaRouche
+ * @since		1.0
+ * @version		1.0
+ */
+package org.game.engine;
---
+++ src/PacMan2/src/game/pacman/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author ryan
+ *
+ */
+package game.pacman;
