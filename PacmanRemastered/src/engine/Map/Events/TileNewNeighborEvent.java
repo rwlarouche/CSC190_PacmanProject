574bb410019bb698f70f0870d98a589459a1af8f
Craig <cmetzdorff1@pride.hofstra.edu>
2018-05-01 18:34:40 -0400

    Added support for Direction to the map tile, but deleted the class because it didn't get committed at first

--- PacmanRemastered/src/engine/Map/Events/TileNewNeighborEvent.java
+++ PacmanRemastered/src/engine/Map/Events/TileNewNeighborEvent.java
@@ -5,6 +5,7 @@
  */
 package engine.Map.Events;
 
+import engine.Direction;
 import engine.Map.Map2DTile;
 import engine.Map.Map2DTileEvent;
 
@@ -14,11 +15,11 @@ import engine.Map.Map2DTileEvent;
  */
 public class TileNewNeighborEvent extends Map2DTileEvent {
     
-    public String direction;
+    public Direction direction;
     public Map2DTile oldNeighbor;
     public Map2DTile newNeighbor;
     
-    public TileNewNeighborEvent(Map2DTile sender, String direction, Map2DTile oldNeighbor, Map2DTile newNeighbor) {
+    public TileNewNeighborEvent(Map2DTile sender, Direction direction, Map2DTile oldNeighbor, Map2DTile newNeighbor) {
         super(sender, "Tile's neighbor has changed!", oldNeighbor, direction, newNeighbor);
         this.direction=direction;
         this.oldNeighbor=oldNeighbor;
