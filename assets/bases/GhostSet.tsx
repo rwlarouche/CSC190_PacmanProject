f4d78c2ac061fc9fed8f180143b42812889afd68
Craig <cmetzdorff1@pride.hofstra.edu>
2018-05-12 17:53:05 -0400

    Finished Ghost spritesheet.

--- assets/bases/GhostSet.tsx
+++ assets/bases/GhostSet.tsx
@@ -1,2 +1,52 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="GhostSet" tilewidth="1" tileheight="1" tilecount="0" columns="0"/>
+<tileset name="GhostSet" tilewidth="64" tileheight="64" tilecount="16" columns="4">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="16">
+  <image width="64" height="64" source="CopyThese/GreenGhost_Down.png"/>
+ </tile>
+ <tile id="17">
+  <image width="64" height="64" source="CopyThese/GreenGhost_Left.png"/>
+ </tile>
+ <tile id="18">
+  <image width="64" height="64" source="CopyThese/GreenGhost_Up.png"/>
+ </tile>
+ <tile id="19">
+  <image width="64" height="64" source="CopyThese/GreenGhost_Right.png"/>
+ </tile>
+ <tile id="20">
+  <image width="64" height="64" source="CopyThese/OrangeGhost_Down.png"/>
+ </tile>
+ <tile id="21">
+  <image width="64" height="64" source="CopyThese/OrangeGhost_Left.png"/>
+ </tile>
+ <tile id="22">
+  <image width="64" height="64" source="CopyThese/OrangeGhost_Up.png"/>
+ </tile>
+ <tile id="23">
+  <image width="64" height="64" source="CopyThese/OrangeGhost_Right.png"/>
+ </tile>
+ <tile id="24">
+  <image width="64" height="64" source="CopyThese/PurpleGhost_Down.png"/>
+ </tile>
+ <tile id="25">
+  <image width="64" height="64" source="CopyThese/PurpleGhost_Left.png"/>
+ </tile>
+ <tile id="26">
+  <image width="64" height="64" source="CopyThese/PurpleGhost_Up.png"/>
+ </tile>
+ <tile id="27">
+  <image width="64" height="64" source="CopyThese/PurpleGhost_Right.png"/>
+ </tile>
+ <tile id="28">
+  <image width="64" height="64" source="CopyThese/RedGhost_Down.png"/>
+ </tile>
+ <tile id="29">
+  <image width="64" height="64" source="CopyThese/RedGhost_Left.png"/>
+ </tile>
+ <tile id="30">
+  <image width="64" height="64" source="CopyThese/RedGhost_Up.png"/>
+ </tile>
+ <tile id="31">
+  <image width="64" height="64" source="CopyThese/RedGhost_Right.png"/>
+ </tile>
+</tileset>
