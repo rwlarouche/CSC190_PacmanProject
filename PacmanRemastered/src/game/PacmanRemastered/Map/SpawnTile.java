dbd3e1b4b816229c2edba4d8f8068ab432e08792
Craig <cmetzdorff1@pride.hofstra.edu>
2018-05-16 23:59:55 -0400

    Started adding new tile type for spawning fruit.

--- PacmanRemastered/src/game/PacmanRemastered/Map/SpawnTile.java
+++ PacmanRemastered/src/game/PacmanRemastered/Map/SpawnTile.java
@@ -5,10 +5,20 @@
  */
 package game.PacmanRemastered.Map;
 
+import engine.Map.Map2DTile;
+
 /**
  *
  * 
  */
 public class SpawnTile extends PacTileEmpty {
     
+    
+    public SpawnTile(Map2DTile up, Map2DTile down, Map2DTile left, Map2DTile right){
+        this();
+    }
+    
+    public SpawnTile(){
+        
+    }
 }
