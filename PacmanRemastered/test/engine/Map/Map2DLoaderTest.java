ed3a1b484e1d6416ba7e42f30a15db6c9a6a5a92
Craig <cmetzdorff1@pride.hofstra.edu>
2018-05-21 21:16:28 -0400

    Scott's branch was being weird with merges, so took the only two files that were actually modified and copied them over manually.

---
+++ PacmanRemastered/test/engine/Map/Map2DLoaderTest.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package engine.Map;
+
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author "[ ]"
+ */
+public class Map2DLoaderTest {
+    
+    public Map2DLoaderTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of translateToTile method, of class Map2DLoader.
+     */
+    @Test
+    public void testTranslateToTile() {
+        System.out.println("translateToTile");
+        char symbol = ' ';
+        int row = 0;
+        int column = 0;
+        Map2DLoader instance = null;
+        Map2DTile expResult = null;
+        Map2DTile result = instance.translateToTile(symbol, row, column);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of loadMap method, of class Map2DLoader.
+     */
+    @Test
+    public void testLoadMap_Map2DBuilder_InputStream() throws Exception{
+        System.out.println("loadMap");
+        Map2DBuilder b = null;
+        InputStream stream = new ByteArrayInputStream("".getBytes(StandardCharsets.UTF_8));
+        Map2DLoader instance = new Map2DLoaderImpl();
+        Map2DTile[][] expResult = null;
+        Map2DTile[][] result = instance.loadMap(b, stream);
+        assertArrayEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of loadMap method, of class Map2DLoader.
+     */
+    @Test
+    public void testLoadMap_Map2DBuilder() throws Exception {
+        System.out.println("loadMap");
+        Map2DBuilder b = null;
+        Map2DLoader instance = new Map2DLoaderImpl();
+        Map2DTile[][] expResult = null;
+        Map2DTile[][] result = instance.loadMap(b);
+        assertArrayEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    public class Map2DLoaderImpl extends Map2DLoader {
+
+        public Map2DLoaderImpl() {
+            super(null);
+        }
+
+        @Override
+        public Map2DTile translateToTile(char symbol, int row, int column) {
+            return new NullTile();
+        }
+    }
+    
+}
